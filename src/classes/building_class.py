0104895c2dd7f31e1f186794086754bba5bceea0
elads <eladsez36@gmail.com>
2021-11-04 16:19:43 +0200

    json path fix

--- src/classes/building_class.py
+++ src/classes/building_class.py
@@ -1,6 +1,5 @@
 import json
 
-
 class Building(object):
     def __init__(self, min_floor: int = None, max_floor: int = None, elevators: list = None):
         self.min_floor = min_floor
@@ -8,7 +7,6 @@ class Building(object):
         self.elevators = elevators
 
     def _init_from_file(self, file_name: str):
-        new_building_dict = {}
         with open(file_name, "r") as f:
             dict_building = json.load(f)
             self.min_floor = dict_building["_minFloor"]
@@ -24,14 +22,14 @@ class Building(object):
 
 if __name__ == '__main__':
     b = Building()
-    b._init_from_file("../input_data/b1.json")
+    b._init_from_file("../../input_data/b1.json")
     print(b)
     print(b.number_of_elevators())
-    # b._init_from_file("../input_data/b2.json")
+    # b._init_from_file("../../input_data/b2.json")
     # print(b)
-    # b._init_from_file("../input_data/b3.json")
+    # b._init_from_file("../../input_data/b3.json")
     # print(b)
-    # b._init_from_file("../input_data/b4.json")
+    # b._init_from_file("../../input_data/b4.json")
     # print(b)
-    # b._init_from_file("../input_data/b5.json")
+    # b._init_from_file("../../input_data/b5.json")
     # print(b)
